c4a4d0dd0181f753ed863fc67a7f95aa6ef7b749
Tirtanusa Kurnia Adhi Perdana <98889173+tirtanusa@users.noreply.github.com>
2026-03-23 14:56:57 +0700

    feat: adding price formatter in course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
+    use HasFactory, SoftDeletes;
+
     protected $fillable =[
         'title',
         'description',
@@ -14,6 +18,8 @@ class Course extends Model
         'current_student',
         'level',
         'status',
+        'instructor_id',
+        'category_id'
     ];
 
     protected $casts = [
@@ -28,4 +34,24 @@ class Course extends Model
         'updated_at',
         'deleted_at'
     ];
+
+    protected $appends = [
+        'formatted_price'
+    ];
+
+    public function instructor(){
+        return $this->belongsTo(User::class, 'instructor_id'); //Memperjelas relasi, sehingga mempermudah ketika mengambil data dari users table
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function lessons(){
+        return $this->hasMany(Lesson::class, 'course_id');
+    }
+
+    public function getFormattedPriceAttribute(){
+        return 'Rp. ' . number_format($this->price, 0, 0, ',', '.');
+    }
 }
